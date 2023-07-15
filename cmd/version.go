4b1d10afea804fb60710c77121ed8453159ad50a
nirev23 <129072324+nirev23@users.noreply.github.com>
2023-07-15 12:14:31 +0300

    Update version.go

--- cmd/version.go
+++ cmd/version.go
@@ -9,6 +9,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// appVersion Application Version
+var appVersion = "Version"
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
